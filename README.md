fe23441dc5bac71705e0c48d88423021e6911e1b
Mick Watkins <mick_watkins94@hotmail.com>
2016-09-26 13:18:29 +0000

    Script now creates a sudo user, readme updated with this info.

--- README.md
+++ README.md
@@ -15,12 +15,14 @@ The purpose of this is to document how to start up a Digital Ocean droplet runni
     # cd ember-start-up
     ```
 
-3. Execute the `startup.sh` file with the name of your new ember app as the first parameter:
+3. Execute the `startup.sh` file with the username of the user you wish to be setup as a `sudoer` as the first parameter, and the name of your new ember app as the second parameter:
 
     ```
-    # ./startup.sh new-app
+    # ./startup.sh username new-app
     ```
 
 4. The ember server is started at the end of the script to test everything worked. Navigate to the url output by the script on the line starting with `##########`, it should now display the 'Welcome to Ember' page.
 
-5. New ember projects can now be created and modified.
+5. A new `sudo` user has now also been created with the username set to what was entered on the command line, and the password is the same.
+
+6. New ember projects can now be created and modified.
--- startup.sh
+++ startup.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
-if [ $# -ne 1 ]
-  then
-    echo "Usage: ./startup.sh FILENAME"
+if [ $# -ne 2 ]
+then
+    echo "Usage: ./startup.sh USERNAME FILENAME"
     exit 1
+fi
+
+if [ "$(id -u)" != "0" ]
+then
+  echo "Must be root to run this script."
+  exit 1
 fi
 
 # Set vim config
@@ -14,6 +20,20 @@ set expandtab
 colorscheme desert
 ENDVIMCONFIG
 ) > ~/.vimrc
+
+# Create user
+username="$1"
+pass="$1"
+grep -q "$username" /etc/passwd
+if [ $? -eq 0 ] 
+then    
+    echo "User $username already exists."
+else
+    useradd -p `mkpasswd "$pass"` -d /home/"$username" -m -g users -s /bin/bash "$username"
+    echo "$username  ALL=(ALL:ALL) ALL" >> /etc/sudoers
+    echo "Account $username is setup as sudoer."
+fi
+
 
 # Create 4G swap file
 ## Without this npm installs fail with 'KILLED'
@@ -34,8 +54,8 @@ npm install -g bower
 npm install -g phantomjs-prebuilt
 
 # Create ember project
-ember new $1 --skip-npm
-cd $1
+ember new $2 --skip-npm
+cd $2
 npm install
 bower install --allow-root
 IP=`/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'`
